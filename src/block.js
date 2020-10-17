e758d144c87361be9add9d80c7420c48651dd8b9
manticorevault <artur_seilah@hotmail.com>
2020-10-17 21:00:06 +0100

    Refactored the code. All tests working properly

--- src/block.js
+++ src/block.js
@@ -13,8 +13,6 @@ const SHA256 = require('crypto-js/sha256');
 const hex2ascii = require('hex2ascii');
 
 class Block {
-
-    // Constructor - argument data will be the object containing the transaction data
     constructor(data) {
         this.hash = null;                                           // Hash of the block
         this.height = 0;                                            // Block Height (consecutive number of each block)
@@ -23,53 +21,55 @@ class Block {
         this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
 
-    /**
-     *  validate() method will validate if the block has been tampered or not.
-     *  Been tampered means that someone from outside the application tried to change
-     *  values in the block data as a consecuence the hash of the block should be different.
-     *  Steps:
-     *  1. Return a new promise to allow the method be called asynchronous.
-     *  2. Save the in auxiliary variable the current hash of the block (`this` represent the block object)
-     *  3. Recalculate the hash of the entire block (Use SHA256 from crypto-js library)
-     *  4. Compare if the auxiliary hash value is different from the calculated one.
-     *  5. Resolve true or false depending if it is valid or not.
-     *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
-     */
-    validate() {
+    validateBlock() {
+
         let self = this;
-        return new Promise((resolve, reject) => {
-            // Save in auxiliary variable the current block hash
-            let currentHash = self.hash
-            self.hash = null
-            // Recalculate the hash of the Block
-            let newHash = SHA256(JSON.stringify(self)).toString()
-            self.hash = currentHash
-            // Comparing if the hashes changed
-            // Returning the Block is not valid
-            // Returning the Block is valid
-            resolve(currentHash === newHash);
+
+        return new Promise(async (resolve, reject) => {
+            let tHash = self.hash;
+            let tBlock = {
+                hash: null,
+                height: self.height,
+                body: self.body,
+                time: self.time,
+                previousBlockHash: self.previousBlockHash
+
+            }
+
+            let hashCalculation = SHA256(JSON.stringify(tBlock)).toString();
+            if (tHash !== hashCalculation) {
+                resolve(false);
+
+            } else {
+                resolve(true);
+
+            }
         });
     }
 
-    /**
-     *  Auxiliary Method to return the block body (decoding the data)
-     *  Steps:
-     *  
-     *  1. Use hex2ascii module to decode the data
-     *  2. Because data is a javascript object use JSON.parse(string) to get the Javascript Object
-     *  3. Resolve with the data and make sure that you don't need to return the data for the `genesis block` 
-     *     or Reject with an error.
-     */
-    getBData() {
-        // Getting the encoded data saved in the Block
-        // Decoding the data to retrieve the JSON representation of the object
-        const decodedData = hex2ascii(this.body);
-        // Parse the data to an object to be retrieve.
-        const dataObj = JSON.parse(decodedData);
-        // Resolve with the data if the object isn't the Genesis block
-        if (dataObj && this.height > 0) {
-            return dataObj;
-        }
+    getBlockData() {
+
+        let self = this;
+
+        return new Promise(async (resolve, reject) => {
+
+            if (self.height === 0) {
+                resolve("Genesis Block");
+
+            }
+
+            let encodedData = this.body;
+            let decodedData = hex2ascii(encodedData);
+            let dataObj = JSON.parse(decodedData);
+
+            if (dataObj) {
+                resolve(dataObj);
+
+            } else {
+                reject("This is an empty block")
+
+            }
+        });
     }
 
 }
